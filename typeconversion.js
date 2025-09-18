01c51916a2559a13acd1e11d4a5877604427783d
wananaysharma42-star <w.ananaysharma42@gmail.com>
2025-09-18 17:45:56 +0000

    type converion topic

---
+++ typeconversion.js
@@ -0,0 +1,19 @@
+let value1 = "Ananay"
+let x1 = Number(value1)
+console.log(value1)
+console.log(x1)
+
+let value2 = 2
+let x2 = String(value2)
+console.log(value2)
+console.log(typeof value2)
+console.log(x2)
+console.log(typeof x2)
+
+let value3 = -1
+let v1 = Boolean(value3)
+console.log(v1)
+
+let value4 = "jhbvfjk"
+let v2 = Boolean(value4)
+console.log(v2)
